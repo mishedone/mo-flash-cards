4df150257e276f22e4c125c13c276d0f8316f8a9
mishedone <michamilev@gmail.com>
2015-01-04 12:48:06 +0200

    Add comments to the API controller.

--- src/Mo/FlashCardsBundle/Controller/ApiController.php
+++ src/Mo/FlashCardsBundle/Controller/ApiController.php
@@ -5,8 +5,15 @@ namespace Mo\FlashCardsBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
+/**
+ * Actions handling the data flow.
+ */
 class ApiController extends Controller
 {
+    /**
+     * @param string $group
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
     public function cardsAction($group)
     {
         $cards = array(
