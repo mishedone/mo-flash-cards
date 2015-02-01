2c7cea1e18e2f3c07ac1c753e51a47099dc2d771
mishedone <michamilev@gmail.com>
2015-02-01 22:26:31 +0200

    Test the learn cards page.

--- src/Mo/FlashCardsBundle/Tests/Controller/LearnControllerTest.php
+++ src/Mo/FlashCardsBundle/Tests/Controller/LearnControllerTest.php
@@ -12,7 +12,20 @@ class LearnControllerTest extends DocumentWebTestCase
         $crawler = $client->request('GET', '/en/learn/decks');
         
         // check the deck link list
-        $this->assertEquals(1, $crawler->filter('a[href="/en/learn/cards/animals"]:contains("Animals")')->count());
-        $this->assertEquals(1, $crawler->filter('a[href="/en/learn/cards/cars"]:contains("Cars")')->count());
-    } 
+        $this->assertEquals(1, $crawler->filter('a[href="/en/learn/cards/animals"]:contains("Animals")')->count(), 'First link is there.');
+        $this->assertEquals(1, $crawler->filter('a[href="/en/learn/cards/cars"]:contains("Cars")')->count(), 'Second link is there.');
+    }
+    
+    public function testCards()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/en/learn/cards/animals');
+        
+        // check if all elements are there
+        $this->assertEquals(1, $crawler->filter('#card-player-question')->count(), 'Questions are shown.');
+        $this->assertEquals(1, $crawler->filter('#card-player-answer')->count(), 'You can answer a question.');
+        $this->assertEquals(1, $crawler->filter('#card-player-show-hint')->count(), 'You can ask for a hint.');
+        $this->assertEquals(1, $crawler->filter('#card-player-hint')->count(), 'Hints are shown.');
+        $this->assertEquals(1, $crawler->filter('a[href="/en/learn/decks"]')->count(), 'You can go back to the decks page.');
+    }
 }
