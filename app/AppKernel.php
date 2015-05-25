fa76693e913eb1aaf2c0e46668457182c749e92d
mishedone <michamilev@gmail.com>
2015-05-25 21:25:51 +0300

    Initialize skeleton bundle for the API.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new Mo\FlashCardsApiBundle\MoFlashCardsApiBundle(),
             new Mo\FlashCardsBundle\MoFlashCardsBundle(),
         );
 
---
+++ src/Mo/FlashCardsApiBundle/MoFlashCardsApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mo\FlashCardsApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MoFlashCardsApiBundle extends Bundle
+{
+}
