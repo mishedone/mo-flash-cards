d8d407a078dddf9f0b56e3c5d205ed070ba2bd3c
mishedone <michamilev@gmail.com>
2015-07-05 13:21:06 +0300

    Prepare basic template for our single page application.

---
+++ src/Mo/FlashCardsBundle/Controller/AppController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Mo\FlashCardsBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * Entry point for our flash cards application.
+ */
+class AppController extends Controller
+{
+    /**
+     * @return Response
+     */
+    public function indexAction()
+    {
+        return $this->render('MoFlashCardsBundle::app.html.twig');
+    }
+}
