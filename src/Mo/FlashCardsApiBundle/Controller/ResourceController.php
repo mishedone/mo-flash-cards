597013cfa6360d5a7a95fed86d2ae858bf1b4869
mishedone <michamilev@gmail.com>
2015-08-16 16:10:17 +0300

    Implement caching of the tts audio files.

--- src/Mo/FlashCardsApiBundle/Controller/ResourceController.php
+++ src/Mo/FlashCardsApiBundle/Controller/ResourceController.php
@@ -2,6 +2,7 @@
 
 namespace Mo\FlashCardsApiBundle\Controller;
 
+use AppKernel;
 use Symfony\Component\HttpFoundation\Response;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
 
@@ -10,6 +11,27 @@ use Nelmio\ApiDocBundle\Annotation\ApiDoc;
  */
 class ResourceController
 {
+    /**
+     * @var AppKernel
+     */
+    protected $kernel;
+
+    /**
+     * @return AppKernel
+     */
+    protected function getKernel()
+    {
+        return $this->kernel;
+    }
+
+    /**
+     * @param AppKernel $kernel
+     */
+    public function __construct(AppKernel $kernel)
+    {
+        $this->kernel = $kernel;
+    }
+
     /**
      * Creates an audio/mpeg pronouncing the text parameter.
      *
@@ -24,13 +46,22 @@ class ResourceController
      */
     public function textToSpeechAction($text)
     {
-        $audio = file_get_contents('https://api.voicerss.org/?' . http_build_query(array(
-            'key' => '36b73c895a48485faedd7f6f8de71eb0',
-            'hl' => 'en-gb',
-            'src' => $text,
-            'c' => 'mp3',
-            'f' => '48khz_16bit_stereo'
-	    )));
+        $key = md5(mb_strtolower($text, 'UTF-8'));
+        $cachePath = $this->getKernel()->getRootDir() . '/tts/' . $key . '.mp3';
+
+        // try loading the audio from cache
+        if (file_exists($cachePath)) {
+            $audio = file_get_contents($cachePath);
+        } else {
+            $audio = file_get_contents('https://api.voicerss.org/?' . http_build_query(array(
+                'key' => '36b73c895a48485faedd7f6f8de71eb0',
+                'hl' => 'en-gb',
+                'src' => $text,
+                'c' => 'mp3',
+                'f' => '48khz_16bit_stereo'
+            )));
+            file_put_contents($cachePath, $audio);
+        }
 
         // build response
         $response = new Response();
