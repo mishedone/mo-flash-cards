2275026add406bb08f032ebdefe692b97eb6aceb
mishedone <michamilev@gmail.com>
2017-02-24 16:48:45 +0200

    Install jms serializer bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JMS\SerializerBundle\JMSSerializerBundle(),
             
             // application bundles
             new AppBundle\AppBundle(),
