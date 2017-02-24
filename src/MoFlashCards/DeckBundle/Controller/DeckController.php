35f31446c832b748765bbb42fc51fb158e52dc1e
mishedone <michamilev@gmail.com>
2017-02-24 17:27:41 +0200

    Implement get deck details endpoint.

--- src/MoFlashCards/DeckBundle/Controller/DeckController.php
+++ src/MoFlashCards/DeckBundle/Controller/DeckController.php
@@ -5,6 +5,7 @@ namespace MoFlashCards\DeckBundle\Controller;
 use JMS\Serializer\SerializationContext;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class DeckController extends Controller
 {
@@ -21,6 +22,32 @@ class DeckController extends Controller
         
         $context = SerializationContext::create()->setGroups(['list']);
         $json = $this->get('jms_serializer')->serialize($decks, 'json', $context);
+        
+        $response = new Response();
+        $response->setContent($json);
+        $response->headers->set('Content-Type', 'application/json');
+        
+        return $response;
+    }
+    
+    /**
+     * Loads a certain deck by it's slug.
+     *
+     * @param string $slug
+     * @return Response
+     */
+    public function getAction($slug)
+    {
+        $deck = $this->get('doctrine_mongodb')
+            ->getRepository('DeckBundle:Deck')
+            ->findOneBySlug($slug);
+            
+        if (!$deck) {
+            throw new NotFoundHttpException();
+        }
+        
+        $context = SerializationContext::create()->setGroups(['details']);
+        $json = $this->get('jms_serializer')->serialize($deck, 'json', $context);
         
         $response = new Response();
         $response->setContent($json);
