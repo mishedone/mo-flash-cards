7f13d8356370b4d24aabd4ba966bfd47459fc442
mishedone <michamilev@gmail.com>
2017-02-28 17:19:14 +0200

    Install and configure nelmio api doc bundle.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\MongoDBBundle\DoctrineMongoDBBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
             
             // application bundles
             new AppBundle\AppBundle(),
