6a79108b3a07f03cae61ec183ebbb050b593a49e
mishedone <michamilev@gmail.com>
2017-10-08 12:12:47 +0300

    Clean up react app default structure.

--- new/src/index.js
+++ new/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+class App extends React.Component {
+    render() {
+        return <div>mo|flash-cards</div>
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
