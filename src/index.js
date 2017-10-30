fd817e528eed6c9f6668dc66604e5dcf68300c23
mishedone <michamilev@gmail.com>
2017-10-31 00:44:32 +0200

    Open learn mode when clicking on front and back buttons.

--- src/index.js
+++ src/index.js
@@ -37,7 +37,9 @@ class App extends React.Component {
             <div>
                 <nav className="navbar navbar-dark bg-dark mb-3">
                     <div className="container-fluid">
-                        <a className="navbar-brand w-100 text-center" href="/">Flash Cards</a>
+                        <a className="navbar-brand w-100 text-center" href="/">
+                            Flash Cards
+                        </a>
                     </div>
                 </nav>
                 <div className="container-fluid">
@@ -52,8 +54,16 @@ class DeckList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            deck: null
+            deck: null,
+            direction: ''
         }
+    }
+    
+    learn(deck, direction) {
+        this.setState({
+            deck: deck,
+            direction: direction
+        });
     }
 
     render() {
@@ -62,14 +72,23 @@ class DeckList extends React.Component {
                 <div className="card mb-3">
                     <div className="card-body text-center">
                         <h4 className="card-title">{deck.name}</h4>
-                        <a href="#" role="button" className="btn btn-success btn-sm">Front</a>
-                        <a href="#" role="button" className="btn btn-dark btn-sm ml-1">Back</a>
+                        
+                        <button type="button" className="btn btn-success btn-sm"
+                                onClick={() => this.learn(deck, 'front')}>
+                            Front
+                        </button>
+                        
+                        <button type="button" className="btn btn-dark btn-sm ml-1"
+                                onClick={() => this.learn(deck, 'back')}>
+                            Back
+                        </button>
                     </div>
                 </div>
             </div>
         ));
 
-        const learn = this.state.deck ? <DeckLearn deck={this.state.deck} /> : '';
+        const learn = this.state.deck ? <DeckLearn deck={this.state.deck}
+                                                   direction={this.state.direction} /> : '';
 
         return (
             <div>
@@ -88,11 +107,20 @@ class DeckList extends React.Component {
 
 class DeckLearn extends React.Component {
     render() {
+        const title = `Learning ${this.props.deck.name} (${this.props.direction})`;
+        const cards = this.props.deck.cards.map((card) => (
+            <li>{card.front} => {card.back}</li>
+        ));
+        
         return (
             <div className="row">
                 <div className="col-12 text-center mb-3">
-                    <h2 className="h2">Learning ...</h2>
+                    <h2 className="h2">{title}</h2>
                 </div>
+                
+                <ul className="list-unstyled">
+                    {cards}
+                </ul>
             </div>
         );
     }
