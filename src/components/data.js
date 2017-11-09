667f1dba4696488afdc6dd0d0a5e5fdd44308734
mishedone <michamilev@gmail.com>
2017-11-09 23:11:15 +0200

    Create dummy data.

---
+++ src/components/data.js
@@ -0,0 +1,29 @@
+const decks = [
+    {
+        name: 'Marketing',
+        slug: 'marketing',
+        cards: [
+            {
+                front: 'John',
+                back: 'Doe'
+            }
+        ]
+    },
+    {
+        name: 'Developers',
+        slug: 'development',
+        cards: []
+    },
+    {
+        name: 'Sales',
+        slug: 'sales',
+        cards: []
+    },
+    {
+        name: 'Support',
+        slug: 'support',
+        cards: []
+    }
+]
+
+export default decks
