ab692e864a3cfc9c0581e93cb7dd26da960e9797
milev <michael.milev@agilemates.com>
2017-12-22 09:44:18 +0200

    install open iconic

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from './router'
 
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'open-iconic/font/css/open-iconic-bootstrap.css'
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
